d77b800c944a1850fecb3d1688d2f86859dc6622
Vital Ozierski <v.ozierski@blak-it.com>
2018-01-03 19:25:00 +0300

    fix locales sentry notification

--- src/errors/TranslationEventHandler.php
+++ src/errors/TranslationEventHandler.php
@@ -2,12 +2,21 @@
 
 namespace blakit\api\errors;
 
+use blakit\api\Module;
 use yii\i18n\MissingTranslationEvent;
 
 class TranslationEventHandler
 {
     public static function handleMissingTranslation(MissingTranslationEvent $event)
     {
-        \Yii::warning('Missed localization "' . $event->message . '" for ' . $event->language . ' [' . $event->category . ']', 'localization');
+        /** @var Module $module */
+        $module = \Yii::$app->controller->module;
+
+        if ($event->language != $module->defaultLocale) {
+            \Yii::warning(
+                'Missed localization "' . $event->message . '" for ' . $event->language . ' [' . $event->category . ']',
+                'localization'
+            );
+        }
     }
 }
