22bc9bd328f464bafa9b0a2466bba07e5ea528c0
Dmitry Marchenko <dmitry_marchenko@inbox.ru>
2018-01-09 19:54:15 +0300

    Add i18n init

--- src/Module.php
+++ src/Module.php
@@ -4,6 +4,7 @@ namespace blakit\api;
 
 use blakit\api\constants\ErrorCode;
 use blakit\api\errors\ErrorHandler;
+use yii\i18n\I18N;
 
 class Module extends \yii\base\Module
 {
@@ -19,10 +20,8 @@ class Module extends \yii\base\Module
 
     public function init()
     {
-        $handler = new ErrorHandler();
-        \Yii::$app->set('errorHandler', $handler);
-        $handler->register();
-
+        $this->initI18n();
+        $this->initErrorHandler();
         parent::init();
     }
 
@@ -33,5 +32,42 @@ class Module extends \yii\base\Module
                 'class' => \yii\filters\Cors::className(),
             ],
         ];
+    }
+
+    public function initI18n()
+    {
+        $translations = [
+            '*' => [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'ru-RU',
+                'on missingTranslation' => [
+                    'blakit\api\errors\TranslationEventHandler',
+                    'handleMissingTranslation'
+                ]
+            ]
+        ];
+
+        $isset_i18n = false;
+        foreach ($this->components as $key => $value) {
+            if (is_a($value, I18N::class)) {
+                $isset_i18n = true;
+                $this->components[$key]->translations = array_merge_recursive_ex($this->components[$key]->translations, $translations);
+            }
+            if ($isset_i18n) break;
+        }
+
+        if (!$isset_i18n) {
+            $this->setComponents(['i18n' => [
+                'class' => I18N::className(),
+                'translations' => $translations,
+            ]]);
+        }
+    }
+
+    public function initErrorHandler()
+    {
+        $handler = new ErrorHandler();
+        \Yii::$app->set('errorHandler', $handler);
+        $handler->register();
     }
 }
