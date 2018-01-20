bab95105319d9d6173c9b409717eee24f226c15e
Dmitry Marchenko <dmitry_marchenko@inbox.ru>
2018-01-20 21:49:26 +0300

    Fix composer scripts

---
+++ src/validators/UserOwnerImageValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace blakit\api\validators;
+
+use blakit\api\models\Image;
+use yii\validators\Validator;
+
+class UserOwnerImageValidator extends Validator
+{
+    public function validateValue($value)
+    {
+        if (is_array($value)) {
+            foreach ($value as $item) {
+                /** @var Image $image */
+                $image = Image::findOne($item);
+                if (!$image || $image->user_id != \Yii::$app->user->id) {
+                    return [\Yii::t('errors', 'Доступ к одной из картинок запрещен'), []];
+                }
+            }
+        } else {
+            /** @var Image $image */
+            $image = Image::findOne($value);
+            if (!$image || $image->user_id != \Yii::$app->user->id) {
+                return [\Yii::t('errors', 'Доступ к картинке запрещен'), []];
+            }
+        }
+
+        return null;
+    }
+}
