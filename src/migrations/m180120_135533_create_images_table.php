4ec8b1a7f3c82b7bd6291678fcdaca16be4ae2a1
Dmitry Marchenko <dmitry_marchenko@inbox.ru>
2018-01-23 20:40:43 +0300

    Update migration

--- src/migrations/m180120_135533_create_images_table.php
+++ src/migrations/m180120_135533_create_images_table.php
@@ -17,83 +17,46 @@ class m180120_135533_create_images_table extends Migration
     {
         // drop indexes and foreign keys with images table, save info in array
         $tableSchema = Yii::$app->db->schema->getTableSchema('{{%images}}');
-        $restore = [];
-
-        foreach (Yii::$app->db->schema->getTableNames() as $name) {
-            $table = Yii::$app->db->schema->getTableSchema($name);
-            foreach ($table->foreignKeys as $key => $value) {
-                if ($value[0] == 'images') {
-                    $from_id = ''; $to_id = '';
-                    foreach ($value as $keyKey => $keyValue) {
-                        if ($keyKey !== 0) {
-                            $from_id = $keyKey;
-                            $to_id = $keyValue;
-                        }
-                    }
-                    $restore[] = [
-                        'index' => $key,
-                        'from_table' => $name,
-                        'to_table' => 'images',
-                        'from_id' => $from_id,
-                        'to_id' => $to_id,
-                    ];
-                    try {
-                        $this->dropForeignKey($key, $name);
-                        $this->dropIndex($key, $name);
-                    } catch (\yii\db\Exception $e) {}
-                }
-            }
-        }
 
         // drop exists table
         if ($tableSchema) {
-            $this->truncateTable('{{%images}}');
-            foreach ($tableSchema->foreignKeys as $key => $value) {
-                try {
-                    $this->dropForeignKey($key, $value[0]);
-                    $this->dropIndex($key, $value[0]);
-                } catch (\yii\db\Exception $e) {}
-            }
-            $this->dropTable('{{%images}}');
-        }
+            // append
+            $this->addColumn('{{%images}}', 'size', $this->integer()->notNull());
+            $this->addColumn('{{%images}}', 'mime', $this->string()->notNull());
+            $this->addColumn('{{%images}}', 'width', $this->integer()->notNull());
+            $this->addColumn('{{%images}}', 'height', $this->integer()->notNull());
+        } else {
+            // create table
+            $this->createTable('{{%images}}', [
+                'id' => $this->primaryKey(),
+                'user_id' => $this->integer()->notNull(),
+                'name' => $this->string(32)->notNull(),
+                'ext' => $this->string(3)->notNull(),
+                'size' => $this->integer()->notNull(),
+                'mime' => $this->string()->notNull(),
+                'width' => $this->integer()->notNull(),
+                'height' => $this->integer()->notNull(),
+            ]);
 
-        // create table
-        $this->createTable('{{%images}}', [
-            'id' => $this->primaryKey(),
-            'user_id' => $this->integer()->notNull(),
-            'name' => $this->string(32)->notNull(),
-            'ext' => $this->string(3)->notNull(),
-            'size' => $this->integer()->notNull(),
-            'mime' => $this->string()->notNull(),
-            'width' => $this->integer()->notNull(),
-            'height' => $this->integer()->notNull(),
-            'created_at' => $this->integer()->notNull(),
-        ]);
-
-        // creates index for column `user_id`
-        $this->createIndex(
-            'idx-images-user_id',
-            '{{%images}}',
-            'user_id'
-        );
-
-        try {
-            // add foreign key for table `users`
-            $this->addForeignKey(
-                'fk-images-user_id',
+            // creates index for column `user_id`
+            $this->createIndex(
+                'idx-images-user_id',
                 '{{%images}}',
-                'user_id',
-                '{{%users}}',
-                'id'
+                'user_id'
             );
-        } catch (\yii\db\Exception $e) {
-            print 'Warning: The database does not contain \'users\' table';
-        }
 
-        // restore indexes and foreign keys
-        foreach ($restore as $value) {
-            $this->addForeignKey($value['index'], $value['from_table'], $value['from_id'], $value['to_table'], $value['to_id']);
-            $this->createIndex($value['index'], $value['from_table'], $value['from_id']);
+            try {
+                // add foreign key for table `users`
+                $this->addForeignKey(
+                    'fk-images-user_id',
+                    '{{%images}}',
+                    'user_id',
+                    '{{%users}}',
+                    'id'
+                );
+            } catch (\yii\db\Exception $e) {
+                print 'Warning: The database does not contain \'users\' table';
+            }
         }
     }
 
