478f6a99866deb804f8cabccafb03f40a5849058
Vital Ozierski <v.ozierski@blak-it.com>
2018-01-26 15:51:43 +0300

    allow to use validator UserOwnerImage in console

--- src/validators/UserOwnerImageValidator.php
+++ src/validators/UserOwnerImageValidator.php
@@ -3,25 +3,38 @@
 namespace blakit\api\validators;
 
 use blakit\api\models\Image;
+use yii\console\Application;
 use yii\validators\Validator;
 
 class UserOwnerImageValidator extends Validator
 {
+    private function checkImage($value)
+    {
+        $is_console = \Yii::$app instanceof Application;
+
+        /** @var Image $image */
+        $image = Image::findOne($value);
+
+        if (!$image || ($is_console == false &&  $image->user_id != \Yii::$app->user->id)) {
+            return [\Yii::t('errors', 'Доступ к картинке ID {id} запрещен', ['id' => $value])];
+        }
+
+        return true;
+    }
+
     public function validateValue($value)
     {
         if (is_array($value)) {
             foreach ($value as $item) {
-                /** @var Image $image */
-                $image = Image::findOne($item);
-                if (!$image || $image->user_id != \Yii::$app->user->id) {
-                    return [\Yii::t('errors', 'Доступ к картинке ID '.$item.' запрещен'), []];
+                $check = $this->checkImage($item);
+                if($check !== true){
+                    return $check;
                 }
             }
         } else {
-            /** @var Image $image */
-            $image = Image::findOne($value);
-            if (!$image || $image->user_id != \Yii::$app->user->id) {
-                return [\Yii::t('errors', 'Доступ к картинке ID '.$value.' запрещен'), []];
+            $check = $this->checkImage($value);
+            if($check !== true){
+                return $check;
             }
         }
 
