5df20bfe522d4843d8ab78e26c625921acbd76fa
Vital Ozierski <v.ozierski@blak-it.com>
2018-06-12 18:36:25 +0300

    1.6.1 - structure "LatLng" added

---
+++ src/structures/LatLng.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace blakit\api\structures;
+
+use yii\base\Model;
+
+class LatLng extends Model
+{
+    public $lat;
+
+    public $lng;
+
+    public function rules()
+    {
+        return [
+            [['lat', 'lng'], 'required'],
+            [['lat'], 'validateLatitude'],
+            [['lng'], 'validateLongitude']
+        ];
+    }
+
+    function validateLatitude($attribute)
+    {
+        $value = str_replace(',', '.', $this->$attribute);
+        if (!preg_match('/^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,20})?))$/', $value)) {
+            $this->addError($attribute, 'Latitude invalid');
+            return false;
+        }
+
+        return true;
+    }
+
+    function validateLongitude($attribute)
+    {
+        $value = str_replace(',', '.', $this->$attribute);
+        if (!preg_match('/^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,20})?))$/', $value)) {
+            $this->addError($attribute, 'Longitude invalid');
+            return false;
+        }
+
+        return true;
+    }
+}
