e81b23bdaeaa138b06d334d463d4da5c20b63d0f
Vital Ozierski <v.ozierski@blak-it.com>
2018-10-30 13:56:49 +0300

    fix crash in authenticated methods without Authorization header

--- src/filters/JwtAuth.php
+++ src/filters/JwtAuth.php
@@ -17,11 +17,17 @@ class JwtAuth extends AuthMethod
         if ($authHeader !== null && preg_match('/^JWT\s+(.*?)$/', $authHeader, $matches)) {
             $identity = $user->loginByAccessToken($matches[1], get_class($this));
             if ($identity === null) {
-                if(!$allowGuest){
+                if (!$allowGuest) {
                     $this->handleFailure($response);
+                    return null;
                 }
             }
             return $identity;
+        }
+
+        if (!$allowGuest) {
+            $this->handleFailure($response);
+            return null;
         }
 
         return true;
